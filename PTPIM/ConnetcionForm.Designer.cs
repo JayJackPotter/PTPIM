56645973949501dea4c2d8f607a5b653cb9e70f4
JayJackPotter <yixuan_zhang@outlook.com>
2015-10-11 23:42:09 +0800

    就酱
    1.ip输入更人性化
    2.增加nickname输入框
    3.我发现我还是完全没弄懂c#里应该怎么写程序= =跟VB差太多了

--- PTPIM/ConnetcionForm.Designer.cs
+++ PTPIM/ConnetcionForm.Designer.cs
@@ -29,54 +29,153 @@
         private void InitializeComponent()
         {
             this.logBox = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.IP1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.connectButton = new System.Windows.Forms.Button();
+            this.IP2 = new System.Windows.Forms.TextBox();
+            this.IP3 = new System.Windows.Forms.TextBox();
+            this.IP4 = new System.Windows.Forms.TextBox();
+            this.point1 = new System.Windows.Forms.Label();
+            this.point2 = new System.Windows.Forms.Label();
+            this.point3 = new System.Windows.Forms.Label();
+            this.nickname_textbox = new System.Windows.Forms.Label();
+            this.nickname = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // logBox
             // 
             this.logBox.FormattingEnabled = true;
-            this.logBox.ItemHeight = 15;
-            this.logBox.Location = new System.Drawing.Point(12, 12);
+            this.logBox.Location = new System.Drawing.Point(9, 10);
+            this.logBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.logBox.Name = "logBox";
-            this.logBox.Size = new System.Drawing.Size(400, 124);
+            this.logBox.Size = new System.Drawing.Size(301, 108);
             this.logBox.TabIndex = 0;
             // 
-            // textBox1
+            // IP1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 213);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(400, 25);
-            this.textBox1.TabIndex = 1;
+            this.IP1.Location = new System.Drawing.Point(74, 183);
+            this.IP1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.IP1.MaxLength = 3;
+            this.IP1.Name = "IP1";
+            this.IP1.Size = new System.Drawing.Size(26, 20);
+            this.IP1.TabIndex = 1;
+            this.IP1.Text = "255";
+            this.IP1.TextChanged += new System.EventHandler(this.IP1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(164, 184);
+            this.label1.Location = new System.Drawing.Point(134, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Target IP";
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(98, 328);
+            this.connectButton.Location = new System.Drawing.Point(74, 284);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(214, 62);
+            this.connectButton.Size = new System.Drawing.Size(160, 54);
             this.connectButton.TabIndex = 3;
             this.connectButton.Text = "Connect!";
             this.connectButton.UseVisualStyleBackColor = true;
             // 
+            // IP2
+            // 
+            this.IP2.Location = new System.Drawing.Point(118, 183);
+            this.IP2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.IP2.Name = "IP2";
+            this.IP2.Size = new System.Drawing.Size(26, 20);
+            this.IP2.TabIndex = 1;
+            this.IP2.Text = "255";
+            this.IP2.TextChanged += new System.EventHandler(this.IP2_TextChanged);
+            // 
+            // IP3
+            // 
+            this.IP3.Location = new System.Drawing.Point(162, 183);
+            this.IP3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.IP3.Name = "IP3";
+            this.IP3.Size = new System.Drawing.Size(26, 20);
+            this.IP3.TabIndex = 1;
+            this.IP3.Text = "255";
+            this.IP3.TextChanged += new System.EventHandler(this.IP3_TextChanged);
+            // 
+            // IP4
+            // 
+            this.IP4.Location = new System.Drawing.Point(206, 183);
+            this.IP4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.IP4.Name = "IP4";
+            this.IP4.Size = new System.Drawing.Size(26, 20);
+            this.IP4.TabIndex = 1;
+            this.IP4.Text = "255";
+            this.IP4.TextChanged += new System.EventHandler(this.IP4_TextChanged);
+            // 
+            // point1
+            // 
+            this.point1.AutoSize = true;
+            this.point1.Location = new System.Drawing.Point(100, 186);
+            this.point1.Name = "point1";
+            this.point1.Size = new System.Drawing.Size(10, 13);
+            this.point1.TabIndex = 4;
+            this.point1.Text = ".";
+            this.point1.Click += new System.EventHandler(this.point1_Click);
+            // 
+            // point2
+            // 
+            this.point2.AutoSize = true;
+            this.point2.Location = new System.Drawing.Point(147, 186);
+            this.point2.Name = "point2";
+            this.point2.Size = new System.Drawing.Size(10, 13);
+            this.point2.TabIndex = 4;
+            this.point2.Text = ".";
+            this.point2.Click += new System.EventHandler(this.point2_Click);
+            // 
+            // point3
+            // 
+            this.point3.AutoSize = true;
+            this.point3.Location = new System.Drawing.Point(191, 186);
+            this.point3.Name = "point3";
+            this.point3.Size = new System.Drawing.Size(10, 13);
+            this.point3.TabIndex = 4;
+            this.point3.Text = ".";
+            // 
+            // nickname_textbox
+            // 
+            this.nickname_textbox.AutoSize = true;
+            this.nickname_textbox.Location = new System.Drawing.Point(132, 222);
+            this.nickname_textbox.Name = "nickname_textbox";
+            this.nickname_textbox.Size = new System.Drawing.Size(55, 13);
+            this.nickname_textbox.TabIndex = 5;
+            this.nickname_textbox.Text = "Nickname";
+            // 
+            // nickname
+            // 
+            this.nickname.Location = new System.Drawing.Point(68, 244);
+            this.nickname.Name = "nickname";
+            this.nickname.Size = new System.Drawing.Size(183, 20);
+            this.nickname.TabIndex = 6;
+            this.nickname.TextChanged += new System.EventHandler(this.nickname_TextChanged);
+            // 
             // ConnetcionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 430);
+            this.ClientSize = new System.Drawing.Size(318, 373);
+            this.Controls.Add(this.nickname);
+            this.Controls.Add(this.nickname_textbox);
+            this.Controls.Add(this.point3);
+            this.Controls.Add(this.point2);
+            this.Controls.Add(this.point1);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.IP4);
+            this.Controls.Add(this.IP3);
+            this.Controls.Add(this.IP2);
+            this.Controls.Add(this.IP1);
             this.Controls.Add(this.logBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "ConnetcionForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -87,9 +186,17 @@
         #endregion
 
         private System.Windows.Forms.ListBox logBox;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox IP1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox IP2;
+        private System.Windows.Forms.TextBox IP3;
+        private System.Windows.Forms.TextBox IP4;
+        private System.Windows.Forms.Label point1;
+        private System.Windows.Forms.Label point2;
+        private System.Windows.Forms.Label point3;
+        private System.Windows.Forms.Label nickname_textbox;
+        private System.Windows.Forms.TextBox nickname;
     }
 }
 
--- PTPIM/ConnetcionForm.cs
+++ PTPIM/ConnetcionForm.cs
@@ -20,5 +20,75 @@ namespace PTPIM
         {
             InitializeComponent();
         }
+
+        private void point2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void point1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IP1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (255 >= Int32.Parse(IP1.Text) && Int32.Parse(IP1.Text )<= 0)
+            {
+                IP.Address.ipAddress1 = Convert.ToByte(IP1.Text);
+            }
+            
+
+        }
+        public static class IP
+        {   public  static bool numberOnly
+            {
+            //我还是不会怎么建一个方法然后在别的textbox里调用= =
+            //需要一个方法限制ip Textbox里只能输入数字= =
+            //private void textBox1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+            //{
+            //  if(e.KeyChar!='\b')//这是允许输入退格键
+            //  {
+            //      if((e.KeyChar<'0')||(e.KeyChar>'9'))//这是允许输入0-9数字
+            //      {
+            //      e.Handled = true;
+            //      }
+            //  }
+            //}
+            //我就是不喜欢用多行注释你打我啊= =
+
+            }
+            public static class Address
+            {
+                public static byte ipAddress1 = new byte();
+                public static byte ipAddress2 = new byte();
+                public static byte ipAddress3 = new byte();
+                public static byte ipAddress4 = new byte();
+            
+            }
+
+        }
+
+        private void IP2_TextChanged(object sender, EventArgs e)
+        {
+            IP.Address.ipAddress2 = Convert.ToByte(IP2.Text);
+        }
+
+        private void IP3_TextChanged(object sender, EventArgs e)
+        {
+            IP.Address.ipAddress3 = Convert.ToByte(IP3.Text);
+        }
+
+        private void IP4_TextChanged(object sender, EventArgs e)
+        {
+            IP.Address.ipAddress4 = Convert.ToByte(IP4.Text);
+        }
+
+        private void nickname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    
     }
 }
